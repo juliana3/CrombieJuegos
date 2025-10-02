8ce1dc5e828d2e9bb025621728f7571d75342e2a
Diego <diegoabelleyra74@gmail.com>
2025-10-02 09:59:58 -0300

    Se realiza el gestor de preguntas. Se puede agregar/editar/eliminar una pregunta, tambien se puede agregar/editar/cambiar la visibilidad/eliminar una categoria, todo funciona al 100%, solamente faltan algunos estilos de las scroll bars y de cancelar/agregar pregunta, tambien se pueden agregar unos pops en el caso de que se quiera eliminar una categoria y una pregunta, ya que solamente aparece un alerta de windows. Se agrego mas endpoints en el back.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,14 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import "../pages/css/home.css";
-import { useNavigate } from "react-router-dom";
 
 export default function Home() {
   const navigate = useNavigate();
 
   return (
     <div className="home-container">
-
       <img className="header-img" src="cropped2.svg" alt="Logo" />
 
-      {/* Main Content - Botón central */}
       <main className="main-content">
         <button
           className="start-game-button"
@@ -18,14 +17,19 @@ export default function Home() {
           Iniciar Juego
         </button>
       </main>
-
-      {/* Footer Section - Botón gestor */}
+     
       <footer className="footer-section">
         <button
           className="gestor-button"
-          onClick={() => navigate()} 
+          onClick={() => navigate("/Preguntas")}
         >
-          Ajustes
+          Gestionar Preguntas
+        </button>
+        <button
+          className='gestor-button'
+          onClick={() => navigate("/GestorPremios")}
+        >
+          Gestionar Premios
         </button>
       </footer>
     </div>
